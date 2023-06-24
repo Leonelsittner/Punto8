2046fbe0b798450daa828caf6be100e22639d39e
leonel Sittner <leonelsittnerp20@gmail.com>
2023-06-24 13:03:34 -0300

    medio creo

--- Form1.vb
+++ Form1.vb
@@ -2,6 +2,12 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim Frase, B As String
         Dim C, A, Contador, F As Integer
+        Frase = txtFrase.Text
+        C = Frase.Length
+        Dim Vfrase(C) As String
+        Contador = 0
+        B = ","
+        A = 0
 
     End Sub
 End Class
