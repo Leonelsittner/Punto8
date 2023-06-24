7f83b225c1dfaa7cf9eb8d8c7bc50794c98417f7
leonel Sittner <leonelsittnerp20@gmail.com>
2023-06-24 13:00:36 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtFrase = New System.Windows.Forms.TextBox()
+        Me.lblMostrar = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(397, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Ejecutar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtFrase
+        '
+        Me.txtFrase.Location = New System.Drawing.Point(26, 45)
+        Me.txtFrase.Name = "txtFrase"
+        Me.txtFrase.Size = New System.Drawing.Size(315, 20)
+        Me.txtFrase.TabIndex = 1
+        '
+        'lblMostrar
+        '
+        Me.lblMostrar.AutoSize = True
+        Me.lblMostrar.Location = New System.Drawing.Point(54, 83)
+        Me.lblMostrar.Name = "lblMostrar"
+        Me.lblMostrar.Size = New System.Drawing.Size(39, 13)
+        Me.lblMostrar.TabIndex = 2
+        Me.lblMostrar.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(549, 121)
+        Me.Controls.Add(Me.lblMostrar)
+        Me.Controls.Add(Me.txtFrase)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtFrase As TextBox
+    Friend WithEvents lblMostrar As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Frase, B As String
+        Dim C, A, Contador, F As Integer
+
+    End Sub
+End Class
